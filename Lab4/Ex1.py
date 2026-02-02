2a036df51cddc2dad8704da0871706d30e84c622
btou22 <btou@hawaii.edu>
2026-02-01 21:04:23 -1000

    Lab4

---
+++ Lab4/Ex1.py
@@ -0,0 +1,12 @@
+# string manipulation examples
+
+first = input("Enter the first name: ")
+middleIn = input("Enter the middle name: ")
+last = input("Enter the last name: ")
+
+full_name = first + " " + middleIn + "." + last
+print("Full name is:", full_name)
+
+print(f"Your full name is: {first} {middleIn}. {last}")
+
+
